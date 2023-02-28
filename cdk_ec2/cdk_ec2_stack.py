4e950cd49f5e789febdd512fe77725bd7b7cf805
Weronika <weronika.piotrowska.pl@gmail.com>
2023-02-28 19:25:56 +0100

    Removed instance type from code - added as github workflow parameter

--- cdk_ec2/cdk_ec2_stack.py
+++ cdk_ec2/cdk_ec2_stack.py
@@ -34,11 +34,3 @@ class CdkEc2Stack(Stack):
         role = iam.Role(self, "InstanceSSM", assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
 
         role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
-
-        # Instance
-        instance = ec2.Instance(self, "Instance",
-                                instance_type=ec2.InstanceType("t3.nano"),
-                                machine_image=amzn_linux,
-                                vpc=vpc,
-                                role=role
-                                )
