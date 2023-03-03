392dda635ed311b5c3d4fe5f697e2d3a9273e838
Weronika <weronika.piotrowska.pl@gmail.com>
2023-03-03 18:58:37 +0100

    Removed instance parameter

--- cdk_ec2/cdk_ec2_stack.py
+++ cdk_ec2/cdk_ec2_stack.py
@@ -9,6 +9,7 @@ from aws_cdk import (
 )
 from constructs import Construct
 
+dirname = os.path.dirname(__file__)
 class CdkEc2Stack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
@@ -35,13 +36,24 @@ class CdkEc2Stack(Stack):
 
         role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name("AmazonSSMManagedInstanceCore"))
 
-        version = self.node.try_get_context("instance")
-
         # Instance
         instance = ec2.Instance(self, "Instance",
-            instance_type=ec2.InstanceType(version),
+            instance_type=ec2.InstanceType("t3.nano"),
             machine_image=amzn_linux,
             vpc = vpc,
             role = role
             )
+
+        # Script in S3 as Asset
+        asset = Asset(self, "Asset", path=os.path.join(dirname, "configure.sh"))
+        local_path = instance.user_data.add_s3_download_command(
+            bucket=asset.bucket,
+            bucket_key=asset.s3_object_key
+        )
+
+        # Userdata executes script from S3
+        instance.user_data.add_execute_file_command(
+            file_path=local_path
+            )
+        asset.grant_read(instance.role)
 
