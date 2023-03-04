c3b01c078bc575ff09fe3b5080217fc8be90ef87
Weronika <weronika.piotrowska.pl@gmail.com>
2023-03-04 13:51:19 +0100

    Added private subnet

--- cdk_ec2/cdk_ec2_stack.py
+++ cdk_ec2/cdk_ec2_stack.py
@@ -17,7 +17,7 @@ class CdkEc2Stack(Stack):
                       nat_gateways=0,
                       subnet_configuration=
                       [ec2.SubnetConfiguration(name="public", subnet_type=ec2.SubnetType.PUBLIC),
-                       ec2.SubnetConfiguration(name="private", subnet_type=ec2.SubnetType.PRIVATE)]
+                       ec2.SubnetConfiguration(name="private", subnet_type=ec2.SubnetType.PRIVATE_WITH_NAT)]
                       )
 
         # AMI
