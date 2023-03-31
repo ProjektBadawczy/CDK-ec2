af189b51540f3afe916e1041a9c70790486b09b7
Weronika <weronika.piotrowska.pl@gmail.com>
2023-03-31 22:07:19 +0200

    Added kernel

--- cdk_ec2/cdk_ec2_stack.py
+++ cdk_ec2/cdk_ec2_stack.py
@@ -25,7 +25,7 @@ class CdkEc2Stack(Stack):
         # AMI
         amzn_linux = ec2.AmazonLinuxImage(
             generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2022,
-            kernel=ec2.AmazonLinuxKernel.KERNEL5_X
+            kernel=ec2.AmazonLinuxKernel.KERNEL_DEFAULT
         )
 
         # Instance Role and SSM Managed Policy
