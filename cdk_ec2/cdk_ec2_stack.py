f1dd7804b12418b86ad7af4b45970a8b012dd122
Weronika <weronika.piotrowska.pl@gmail.com>
2023-03-31 21:52:28 +0200

    Fixed virtualization parameter for new amazon linux 2022

--- cdk_ec2/cdk_ec2_stack.py
+++ cdk_ec2/cdk_ec2_stack.py
@@ -23,9 +23,8 @@ class CdkEc2Stack(Stack):
                       )
 
         # AMI
-        amzn_linux = ec2.MachineImage.latest_amazon_linux(
-            generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2022,
-            edition=ec2.AmazonLinuxEdition.STANDARD
+        amzn_linux = ec2.AmazonLinuxImage(
+            generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2022
         )
 
         # Instance Role and SSM Managed Policy
