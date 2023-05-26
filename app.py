e53e20c9e7e4a5d40f2e43c11a1f68e3fbe8cb75
Weronika Piotrowska <weronika.piotrowska.pl@gmail.com>
2023-05-26 13:28:40 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ from cdk_ec2.cdk_ec2_stack import CdkEc2Stack
 
 
 app = cdk.App()
-CdkEc2Stack(app, "CdkEc2Stack", env=cdk.Environment(account='736936197866', region='eu-central-1'))
+CdkEc2Stack(app, "CdkEc2Stack", env=cdk.Environment(account='736936197866', region='us-east-1'))
 app.synth()
